658ec2b5e2e0135ed1eb222ca68aab840e7451bc
Ariadna <aria0401@kea.stud.dk>
2020-11-18 11:02:50 +0100

    merge conflict solved

--- src/modules/rest.js
+++ src/modules/rest.js
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
 
-
-
-export function get(callback){
-
-    fetch("https://databasesuperheros-0cba.restdb.io/rest/cards", {
-        method: "get",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "x-apikey": "5f96ac894b77c1637d147e06",
-          "cache-control": "no-cache"
-        }
-      })
-        .then(e => e.json())
-        .then(data=>callback(data));
-
-}
-
-=======
 export function get(callback){
     fetch("https://fe2020autumn-8542.restdb.io/rest/cards", {
       method: "get",
@@ -45,4 +26,4 @@ export function post(payload, cards, callback){
           callback(cards.concat(data))
       });
 } 
->>>>>>> 3388f24d658de8beca86d925913fda1672488dd8
+
